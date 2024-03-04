3b821f67f6f2a25b30e5f1534c6245debf4e41a2
yameilama <144568933+yameilama@users.noreply.github.com>
2024-03-04 14:02:12 -0500

    first commit

---
+++ src/main/java/com/lamayamei/pruebadosviamatica/Viamatica/prueba2/ViamaticaPrueba2Application.java
@@ -0,0 +1,13 @@
+package com.lamayamei.pruebadosviamatica.Viamatica.prueba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViamaticaPrueba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViamaticaPrueba2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamayamei/pruebadosviamatica/Viamatica/prueba2/ViamaticaPrueba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamayamei.pruebadosviamatica.Viamatica.prueba2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViamaticaPrueba2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
